78b57e41cd584015e2f7ce662275a52f39eaeb53
Waqas Ali <waqas.ali501@yahoo.com>
2025-01-29 19:18:03 +0500

    feat: add account app, add urls for token creation, refresh token and logout, add requirements.txt file.

--- tasks/urls.py
+++ tasks/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('apis/', include('apis.urls')),
 ]
+
+# swagger UI
+# http://127.0.0.1:8000/apis/schema/swagger-ui/
